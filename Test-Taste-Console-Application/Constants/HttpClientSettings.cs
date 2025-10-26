7c3e3c37123e490c3b5e13ac5b491233722e7abd
Tushar Sharma <tushar.sharma18@outlook.com>
2025-10-26 16:26:23 +0530

    Added the authentication in the httpClient calls | Added APIKey to the HttpClientSettings and applied it to HttpClientService.

--- Test-Taste-Console-Application/Constants/HttpClientSettings.cs
+++ Test-Taste-Console-Application/Constants/HttpClientSettings.cs
@@ -3,5 +3,14 @@
     public static class HttpClientSettings
     {
         public const string JsonType = "application/json";
+
+        public const string AuthType = "Bearer";
+
+        //[Tushar sharma] - 26th oct 2025 
+        // API key to authorize the API calls.
+        // Got this from the same API provided in the doc. 
+        // Below is the source of key.
+        // https://api.le-systeme-solaire.net/en/generatekey.html
+        public const string APIKey = "2e53ac3a-59b7-4c2c-a1b5-e755e0987430";
     }
 }
